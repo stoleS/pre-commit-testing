a5fd45cf2ab942b4fb57c13c5ec01b4318e2c811
stoleS <predragstosic29@gmail.com>
2020-04-16 20:25:16 +0200

    Changed check order

--- scripts/run-checks.sh
+++ scripts/run-checks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo '-> running tests...'
-npm run test || exit 1
-echo 'done'
-
 echo '-> running lint...'
 npm run lint || exit 1
+echo 'done'
+
+echo '-> running tests...'
+npm run test || exit 1
 echo 'done'
