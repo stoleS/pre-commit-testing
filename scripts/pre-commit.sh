d18d279caeee6cb935adaeb01cf79d48b763b3e4
stoleS <predragstosic29@gmail.com>
2020-04-16 19:47:56 +0200

    Test

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-git stash -q --keep-index
-sh $PWD/scripts/run-tests.sh
-RESULT=$?
-git stash pop -q
-[ $RESULT -ne 0 ] && exit 1
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save -q --keep-index $STASH_NAME
+
+sh $PWD/scripts/run-checks.sh
+TEST_RESULT=$?
+
+STASHES=$(git stash list)
+if [[ $STASHES == "$STASH_NAME" ]]; then
+  git stash pop -q
+fi
+
+[ $TEST_RESULT -ne 0 ] && exit 1
 exit 0
 
