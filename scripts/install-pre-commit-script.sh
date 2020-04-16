5024814c03d8dc9dc17fb47bd379accd8c5e3f79
stoleS <predragstosic29@gmail.com>
2020-04-16 14:59:31 +0200

    Updated scripts

--- scripts/install-pre-commit-script.sh
+++ scripts/install-pre-commit-script.sh
@@ -1,3 +1,4 @@
 echo 'installing pre-commit script...'
-ln -sfn ./pre-commit.sh ../.git/hooks/pre-commit 
-chmod +x ../.git/hooks/pre-commit
+cp ./scripts/pre-commit.sh .git/hooks/pre-commit
+echo 'making it executable...'
+chmod +x .git/hooks/pre-commit
