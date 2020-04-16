35f04aabe9ede131f7cef394db3307ba913237d7
stoleS <predragstosic29@gmail.com>
2020-04-16 19:52:41 +0200

    TEST

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 STASH_NAME="pre-commit-$(date +%s)"
+echo $STASH_NAME
 git stash save -q --keep-index $STASH_NAME
 
 #sh $PWD/scripts/run-checks.sh
 TEST_RESULT=$?
 
 STASHES=$(git stash list)
+echo $STASHES
 if [[ $STASHES == "$STASH_NAME" ]]; then
   git stash pop -q
 fi
