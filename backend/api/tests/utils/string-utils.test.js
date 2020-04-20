0829e7d713e161ff89d58af66dcc6902228aabdd
stoleS <predragstosic29@gmail.com>
2020-04-20 22:41:15 +0200

    test

--- backend/api/tests/utils/string-utils.test.js
+++ backend/api/tests/utils/string-utils.test.js
@@ -2,7 +2,7 @@ const { sayHello } = require('../../utils/string-utils')
 
 describe('String utils tests', () => {
   it('should return provided name in message', () => {
-    const names = ['Predrag', 'Milos', 'Djordje', 'Petar']
+    const names = ['Predrag', 'Milos', 'Djordje']
 
     names.forEach(name => {
       const res = sayHello(name)
